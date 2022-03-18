11f184ada27c6a8210b74ebf98c3b75c6638d69f
v.pronina <v.pronina@g.nsu.ru>
2022-03-18 19:11:49 +0700

    added func

--- biopython/task2/clock.py
+++ biopython/task2/clock.py
@@ -1,5 +1,18 @@
 from Bio import Entrez
 
+def GetmRNASeq(search, name):
+    handle = Entrez.esearch(db="nucleotide", term=search)
+    record = Entrez.read(handle)
+    ids = record['IdList']
+
+    seqID = ids[0]
+    handle = Entrez.efetch(db="nucleotide", id=seqID, rettype="fasta", retmode="text")
+    record = handle.read()
+
+    filename = "out/" + name + ".fasta"
+    with open(filename, "w") as file:
+        file.write(record.rstrip("\n"))
+
 Entrez.email = "enjiterin@gmail.com"
 
 human = "Homo sapien"
@@ -10,32 +23,6 @@ searchHuman = "Clock[Gene] AND " + human + "[Organism] AND mRNA[Filter]"
 searchMouse = "Clock[Gene] AND " + mouse + "[Organism] AND mRNA[Filter]"
 searchRat = "Clock[Gene] AND " + rat + "[Organism] AND mRNA[Filter]"
 
-handle = Entrez.esearch(db="nucleotide", term=searchHuman)
-record = Entrez.read(handle)
-ids = record['IdList']
-
-seqID = ids[0]
-handle = Entrez.efetch(db="nucleotide", id=seqID, rettype="fasta", retmode="text")
-record = handle.read()
-with open("out/clockHuman.fasta", "w") as file:
-    file.write(record.rstrip("\n"))
-
-handle = Entrez.esearch(db="nucleotide", term=searchMouse)
-record = Entrez.read(handle)
-ids = record['IdList']
-
-seqID = ids[0]
-handle = Entrez.efetch(db="nucleotide", id=seqID, rettype="fasta", retmode="text")
-record = handle.read()
-with open("out/clockMouse.fasta", "w") as file:
-    file.write(record.rstrip("\n"))
-
-handle = Entrez.esearch(db="nucleotide", term=searchRat)
-record = Entrez.read(handle)
-ids = record['IdList']
-
-seqID = ids[0]
-handle = Entrez.efetch(db="nucleotide", id=seqID, rettype="fasta", retmode="text")
-record = handle.read()
-with open("out/clockRat.fasta", "w") as file:
-    file.write(record.rstrip("\n"))
+GetmRNASeq(searchHuman, "clockHuman")
+GetmRNASeq(searchMouse, "clockMouse")
+GetmRNASeq(searchRat, "clockRat")
